44cf2cab7ccf81f2732f56c2eed06d67a8e5882d
Eusébio Alves <eusebio.a95@hotmail.com>
2023-11-03 12:01:40 +0000

    Implementing error response handling
    This API returns an error object that I will use to decide what to do
    in case of error.

--- js/script.js
+++ js/script.js
@@ -3,16 +3,32 @@ const searchIpt = document.querySelector('input')
 
 
 const getWeatherData = async (searchTerm) => {
-    
-    if (!searchTerm) searchTerm = 'auto:ip'
 
-    const weatherPromise = await fetch('https://api.weatherapi.com/v1/forecast.json?key=fc12493872cf47e9b0c132053233110&days=3&q=' + searchTerm, 
-    { mode: 'cors' })
+    try {
     
-    const weatherData = await weatherPromise.json()
+        if (!searchTerm) searchTerm = 'auto:ip'
 
-    console.log(weatherData)
-    return weatherData
+        const weatherPromise = await fetch('https://api.weatherapi.com/v1/forecast.json?key=fc12493872cf47e9b0c132053233110&days=3&q=' + searchTerm, 
+        { mode: 'cors' })
+        
+        const weatherData = await weatherPromise.json()
+
+        if (weatherData.error) handleError(weatherData.error.code)
+
+        return weatherData
+        
+    } catch(error) {
+
+        console.log(error)
+
+    }
+
+}
+
+const handleError = (errorCode) => {
+
+    console.log('Fixing ' + errorCode)
+
 }
 
 searchBtn.addEventListener('click', () => getWeatherData(searchIpt.value))
