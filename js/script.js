6c965c6a34286596e99da350eb1a47ea3fd103a9
Eusébio Alves <eusebio.a95@hotmail.com>
2023-11-03 09:45:04 +0000

    Weather API real-time data request

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('hey')
+
+
+
+
+const getRealtimeData = async (searchTerm) => {
+    const realtimePromise = await fetch('https://api.weatherapi.com/v1/current.json?key=fc12493872cf47e9b0c132053233110&q=auto:ip')
+    const realtimeData = await realtimePromise.json()
+    return realtimeData
+}
+
+
+getRealtimeData()
+
+
