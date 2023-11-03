bac1184954e54ddcde99a23fdbb6d416803cfaa1
Eusébio Alves <eusebio.a95@hotmail.com>
2023-11-03 10:33:13 +0000

    Better fetch configuration
    Swapped the API method to forecast as it also includes today. Added
    option for user to search for a country.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
+const searchBtn = document.querySelector('button')
+const searchIpt = document.querySelector('input')
 
 
+const getWeatherData = async (searchTerm) => {
+    
+    if (!searchTerm) searchTerm = 'auto:ip'
 
+    const weatherPromise = await fetch('https://api.weatherapi.com/v1/forecast.json?key=fc12493872cf47e9b0c132053233110&days=3&q=' + searchTerm, 
+    { mode: 'cors' })
+    
+    const weatherData = await weatherPromise.json()
 
-const getRealtimeData = async (searchTerm) => {
-    const realtimePromise = await fetch('https://api.weatherapi.com/v1/current.json?key=fc12493872cf47e9b0c132053233110&q=auto:ip')
-    const realtimeData = await realtimePromise.json()
-    return realtimeData
+    console.log(weatherData)
+    return weatherData
 }
 
+searchBtn.addEventListener('click', () => getWeatherData(searchIpt.value))
 
-getRealtimeData()
+
+
 
 
